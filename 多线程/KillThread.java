8f15c162c3c0fa478cbdcb90381cb857a3096465
小邱 <475061976@qq.com>
2020-05-22 15:19:39 +0800

    测试分支提交

---
+++ 多线程/KillThread.java
@@ -0,0 +1,23 @@
+/**
+ * @Author: QiuhaoRan
+ * @Description:
+ * @Date: Created in 14:58 2020/5/22
+ * @Modified By:
+ */
+//继承Thread
+public class KillThread extends Thread{
+
+    private Hero h1;
+    private Hero h2;
+
+    public KillThread(Hero h1, Hero h2){
+        this.h1 = h1;
+        this.h2 = h2;
+    }
+// 重写run方法
+    public void run(){
+        while(!h2.isDead()){
+            h1.attackHero(h2);
+        }
+    }
+}
--- 多线程/test.java
+++
@@ -1,8 +0,0 @@
-/**
- * @Author: QiuhaoRan
- * @Description:
- * @Date: Created in 20:15 2020/5/21
- * @Modified By:
- */
-public class test {
-}
