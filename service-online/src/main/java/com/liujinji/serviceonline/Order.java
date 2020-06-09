c1f23ba302f8f2f0c65e9488beb4c1fbb9d89361
KevinLJJ <ljj0452@gmail.com>
2020-06-09 17:06:39 +0800

    add order validation.

--- service-online/src/main/java/com/liujinji/serviceonline/Order.java
+++ service-online/src/main/java/com/liujinji/serviceonline/Order.java
@@ -1,14 +1,31 @@
 package com.liujinji.serviceonline;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class Order {
+    @NotBlank(message = "姓名不能为空！")
     private String name;
+
+    @NotBlank(message = "省份不能为空！")
     private String province;
+
+    @NotBlank(message = "城市不能为空！")
     private String city;
+
+    @NotBlank(message = "详细地址不能为空！")
     private String address;
+
+    @CreditCardNumber(message = "错误的信用卡号")
     private String ccNumber;
+
+    @NotBlank(message = "有效日期不能为空！")
     private String ccExpiration;
+
+    @Digits(integer = 3,fraction = 0,message = "请输入3位CVV")
     private String ccCVV;
 }
--- service-online/src/main/java/com/liujinji/serviceonline/web/OrderController.java
+++ service-online/src/main/java/com/liujinji/serviceonline/web/OrderController.java
@@ -4,21 +4,28 @@ import com.liujinji.serviceonline.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
-    @GetMapping("/current")
+    @GetMapping()
     public String oderForm(Model model){
         model.addAttribute("order",new Order());
         return "orderForm";
     }
     @PostMapping
-    public String orderProcess(Order orderInfo){
+    public String orderProcess(@Valid Order orderInfo, Errors errors){
+        if (errors.hasErrors()){
+            log.warn("数据格式错误");
+            return "/orderForm";
+        }
         log.info(orderInfo.toString());
         return "/orderForm";
     }
