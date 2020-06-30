ea831279aea7e10e335eab85c48239caa78bc081
SunJinjie <592513724@qq.com>
2020-06-30 13:18:13 +0800

    first commit

--- src/com/spdb/study/design/pattern/factory/factorymethod/animal/AnimalFactory.java
+++
@@ -1,9 +0,0 @@
-package com.spdb.study.design.pattern.factory.factorymethod.animal;
-
-/**
- * @author Mr.Longyx
- * @date 2020年06月21日 16:20
- */
-public interface AnimalFactory {
-    CollectAnimal collectAnimal();
-}
