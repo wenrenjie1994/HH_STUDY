f565b3819149471242dcfd652a61d527a780cd5c
lin-ziyi <15902886144@163.com>
2020-06-30 19:22:14 +0800

    IDE

---
+++ IDE-IDEA/basic-code/day04-code/src/linziyi/HelloWorld.java
@@ -0,0 +1,8 @@
+package linziyi;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!1111");
+        System.out.println("Hello,world!2222");
+    }
+}
