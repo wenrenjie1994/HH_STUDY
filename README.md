9b158d0c6f67cd3f8f6ee815b48f84cb570147fd
fuaowei <Aowei_fu@163.com>
2020-07-02 15:39:54 +0800

    HR系统第一阶段，增删改查，内存简历数据库

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
             2、springBoot的学习
             3、大型网站架构学习
             4、HR系统简易快速入门、第一阶段、基于内存和控制台的简历系统
+
+    7-2 第4分支：HR系统第一阶段基本完成，增加简历的增删改查功能。
+                ArrayList<Resume> resumeList 存储简历对象
+                分层架构 pojo:Resume简历对象
+                        service:createResume、deleteResume、updateResume、selectResume;
+                BUG: 循环访问控制台输入流时，存在不等待输入的情况
+                    int choice = scanner.nextInt()-> java.util.NoSuchElementException
+
+                   
--- resume_Console/src/com/spdb/Main.java
+++ resume_Console/src/com/spdb/Main.java
@@ -1,22 +1,33 @@
 package com.spdb;
 
+import com.spdb.common.ResumeStatusCode;
+import com.spdb.common.ServerResponse;
 import com.spdb.pojo.Resume;
+import com.spdb.service.impl.IResumeServiceImpl;
 import com.spdb.util.IResumeCheck;
 
+import java.rmi.ServerError;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *@Author: A wei
- *@Description 测试java8的Lambda表达式，优化简历检查
+ *@Description 程序流程引导类
  *@return
  *@param
  **/
 public class Main {
 
+    private static IResumeServiceImpl iResumeService = new IResumeServiceImpl();
+
+    private static ArrayList<Resume> resumeArrayList = new ArrayList<>();
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 	// write your code here
-
         Resume resume1 = new Resume( "fuaowei",
-                                    "420621199506128612",
+                                    "420621199506127746",
                                     "scu",
                                     "optical enginering",
                                     0,
@@ -33,7 +44,6 @@ public class Main {
                                     "zhansan@163.com",
                                     -1);
 
-        ArrayList<Resume> resumeArrayList = new ArrayList<>();
         resumeArrayList.add(resume1);
         resumeArrayList.add(resume2);
 
@@ -48,28 +58,142 @@ public class Main {
                                     -1);
         resumeArrayList.add(resume3);
 
-        if (checkResume(resume3,resumeArrayList,
-                (Resume res1,Resume res2)-> res1.getId().equals(res2.getId()))){
-                System.out.println("身份证号码已注册！");
-        }else{
-            System.out.println("身份证号码未注册！");
-        }
-        if (checkResume(resume3,resumeArrayList,
-                (Resume res1,Resume res2)-> res1.getPhone().equals(res2.getPhone()))){
-            System.out.println("电话号码已注册！");
-        }else{
-            System.out.println("电话号码未注册！");
-        }
+
+        //3、根据输入选项进行业务操作
+        new Main().doSomething2Resume();
+
+        scanner.close();
 
     }
 
-    private static boolean checkResume(Resume checkResume, ArrayList<Resume> resumeList, IResumeCheck iResumeCheck){
+    private static void menu(){
+        System.out.println("-------Welcome to the spdb first version HR resume system-------");
+        System.out.println("if you have something to do,choose one. ");
+        System.out.println("---creat a new resume,input 1");
+        System.out.println("---delete a resume,input 2");
+        System.out.println("---update a resume,input 3");
+        System.out.println("---inquire a resume,input 4");
+        System.out.println("---exit system,input 5");
+    }
 
-        for (Resume item:resumeList){
-            if (iResumeCheck.checked(item,checkResume)){
-                return true;
+    private static void doSomething2Resume(){
+
+        while(true){
+
+            menu(); //主菜单
+
+            int choice = scanner.nextInt();
+
+            switch (choice){
+                case 1:{
+                    Resume resume = getResumeInformation(1);
+                    ServerResponse serverResponse = iResumeService.createResume(resume,resumeArrayList);
+                    if (serverResponse.getStatus() < 0){
+                        System.out.println(serverResponse.getMessage());
+                    }else{
+                        System.out.println("resume has been created!\n");
+                    }
+                    break;
+                }
+                case 2:{
+                    Resume resume = (Resume) getUserResumeById(2).getData();
+                    ServerResponse serverResponse = iResumeService.deleteResume(resume,resumeArrayList);
+                    if (serverResponse.getStatus() < 0){
+                        System.out.println(serverResponse.getMessage());
+                    }else{
+                        System.out.println("resume has been deleted!\n");
+                    }
+                    break;
+                }
+                case 3:{
+                    Resume resume = getResumeInformation(3);
+                    ServerResponse serverResponse = iResumeService.updateResume(resume,resumeArrayList);
+                    if (serverResponse.getStatus() < 0){
+                        System.out.println(serverResponse.getMessage());
+                    }else{
+                        System.out.println("resume has been update!\n");
+                    }
+                    break;
+                }
+                case 4:{
+                    String id = (String)getUserResumeById(4).getData();
+                    ServerResponse serverResponse = iResumeService.selectResume(id,resumeArrayList);
+                    if (serverResponse.getStatus() < 0){
+                        System.out.println(serverResponse.getMessage());
+                    }else{
+                        Resume result = (Resume) serverResponse.getData();
+                        printResume(result);
+                    }
+                    break;
+                }
+                case 5: {
+                    System.out.println("bye bye!");
+                    break;
+                }
+                default:
+                    System.out.println("please enter the right option!");
+                    break;
             }
         }
-        return false;
     }
+
+
+    private static Resume getResumeInformation(int status){
+        if(status == 1){
+                System.out.println("-------welcome to create resume-------");
+                System.out.println("please type your: name、id、school、major、sex、phone、email");
+        }else if(status == 3){
+                System.out.println("-------welcome to update resume-------");
+                System.out.println("please type your name、id、school、major、sex、phone、email");
+        }
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        String id = scanner.nextLine();
+        String school = scanner.nextLine();
+        String major = scanner.nextLine();
+        String sexStr = scanner.nextLine();
+        int sex = "男".equals(sexStr)? 0:1;
+        String phone = scanner.nextLine();
+        String email = scanner.nextLine();
+
+        scanner.close();
+
+        Resume result = new Resume(name,id,school,major,sex,phone,email, ResumeStatusCode.NOTAPPLY.getCode());
+
+        return result;
+    }
+
+    private static ServerResponse getUserResumeById(int status){
+        switch (status){
+            case 2:{
+                System.out.println("-------welcome to delete resume-------");
+                System.out.println("please type your: id");
+                Scanner scanner = new Scanner(System.in);
+                String id = scanner.nextLine();
+                scanner.close();
+                return ServerResponse.createBySuccess(iResumeService.selectResumeById(id,resumeArrayList));
+            }
+            case 4:{
+                System.out.println("-------welcome to inquire resume-------");
+                System.out.println("please type your: id");
+                Scanner scanner = new Scanner(System.in);
+                String id = scanner.nextLine();
+                scanner.close();
+                return ServerResponse.createBySuccess(id);
+            }
+        }
+
+        return null;
+    }
+
+    private static void printResume(Resume result){
+        System.out.println("name:"+result.getName()+"\n" +
+                "id:" + result.getId()+"\n"+
+                "school:" + result.getSchool()+"\n"+
+                "major:" + result.getMajor() + "\n"+
+                "sex:" + (result.getSex() == 0?"男":"女") + "\n"+
+                "phone:" + result.getPhone() + "\n"+
+                "email:" + result.getEmail() + "\n");
+    }
+
 }
