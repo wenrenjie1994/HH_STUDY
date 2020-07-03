ae5f0618f852cf747e82739cbb104ec9a0eb7285
yearmonster <564173731@qq.com>
2020-07-03 12:58:14 +0800

    新增笔记

---
+++ java笔记/java第二章/JAVA方法/Demo3Method.java
@@ -0,0 +1,24 @@
+
+public class Demo3Method{
+	public static void main(String[] args){
+		boss();
+		me();
+	}
+	
+	
+	public static void me(){
+		System.out.println("学习");
+		System.out.println("工作");
+		System.out.println("完成任务");
+		System.out.println("挣钱");
+	}
+	
+	public static void boss(){
+		System.out.println("招聘");
+		System.out.println("录用我");
+		System.out.println("布置工作");
+		System.out.println("发工资");
+		
+		
+	}
+}
---
+++ java笔记/java第二章/JAVA运算符/Demo2.java
@@ -0,0 +1,25 @@
+
+
+
+public class Demo2{
+	public static void main (String[] args){
+		int a = 10;
+		a+=5;
+		System.out.println(a);
+		int x=10;
+		x%=3;
+		System.out.println(x);
+		
+		byte num = 30;
+		num +=5;
+		System.out.println(num);
+		
+		System.out.println(true&&false);
+		
+		int num1 = 10;
+		int num2 = 20;
+		int  max;
+		max = num1>num2?num1:num2;
+		System.out.println("最大值为："+max);
+	}
+}
