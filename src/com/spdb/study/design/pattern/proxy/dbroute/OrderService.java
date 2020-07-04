6a49cb065f2cc487740496ceb3c7dfa02c5fd0af
IDcard51373 <ldqhitler@qq.com>
2020-07-04 19:18:35 +0800

    clear trash files

--- src/com/spdb/study/design/pattern/proxy/dbroute/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.spdb.study.design.pattern.proxy.dbroute;
-
-/**
- * @author Mr.Longyx
- * @date 2020年06月26日 20:47
- */
-public interface OrderService {
-    int createOrder(Order order);
-}
