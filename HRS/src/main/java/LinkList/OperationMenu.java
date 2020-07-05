1197fc68121b695811d4a676fd56ae8aed2faea1
KevinLJJ <ljj0452@gmail.com>
2020-07-05 10:56:41 +0800

    完成第一版本HRS

--- HRS/src/main/java/LinkList/OperationMenu.java
+++ HRS/src/main/java/LinkList/OperationMenu.java
@@ -2,26 +2,69 @@ package LinkList;
 
 import bean.Student;
 
+import java.util.Scanner;
+
 /**
  * @author：ljj0452@gmail.com
  * @time: 2020/7/4 22:47
  */
 public class OperationMenu {
 
+    public static void showMenu() {
+        System.out.println("*****************************************");
+        System.out.println("*********** Student Manager V1.0 ********");
+        System.out.println("*****************************************");
+        System.out.println("*** 1. show all students              ***");
+        System.out.println("*** 2. add a student                  ***");
+        System.out.println("*** 3. delete a student by index      ***");
+        System.out.println("*** 4. update a student by index      ***");
+        System.out.println("*** 0. exit                           ***");
+        System.out.println("*****************************************");
+    }
+
+    public static Student buildStudent() {
+        Scanner sc = new Scanner(System.in);
+        Student student = new Student();
+        System.out.print("输入学生ID：");
+        student.setStuID(sc.next());
+        System.out.print("输入学生姓名：");
+        student.setName(sc.next());
+        System.out.print("输入学生年龄：");
+        student.setAge(sc.nextInt());
+        System.out.print("输入学生专业：");
+        student.setMajor(sc.next());
+        System.out.print("输入学生面试成绩：");
+        student.setInterviewScore(sc.nextInt());
+        return student;
+    }
+
     public static void main(String[] args) {
-        Student student1 = new Student("100","Kevin1",22,"IOT",100);
-        Student student2 = new Student("100","Kevin2",22,"IOT",100);
-        Student student3 = new Student("100","Kevin3",22,"IOT",100);
-        Student student4 = new Student("100","Kevin4",22,"IOT",100);
-        Student student5 = new Student("100","Kevin5",22,"IOT",100);
-        StudentSvc svc=new StudentSvc();
-        svc.addStudent(student1);
-        svc.addStudent(student2);
-        svc.addStudent(student3);
-        svc.addStudent(student4);
-        svc.addStudent(student5);
-        svc.showAllStudents();
-        svc.delStudentByIndex(2);
-        svc.showAllStudents();
+        Student student = new Student("123", "Kevin", 11, "iot", 222);
+        Scanner scanner = new Scanner(System.in);
+        StudentSvc svc = new StudentSvc();
+        showMenu();
+        while (true) {
+            System.out.print("Type a number to select: ");
+            switch (scanner.nextInt()) {
+                case 1:
+                    svc.showAllStudents();
+                    break;
+                case 2:
+                    svc.addStudent(buildStudent());
+                    break;
+                case 3:
+                    System.out.print("输入删除索引：");
+                    svc.delStudentByIndex(scanner.nextInt());
+                    break;
+                case 4:
+                    System.out.print("输入修改索引：");
+                    svc.updateStudentByIndex(scanner.nextInt(), buildStudent());
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    showMenu();
+            }
+        }
     }
 }
--- HRS/src/main/java/LinkList/StudentSvc.java
+++ HRS/src/main/java/LinkList/StudentSvc.java
@@ -2,19 +2,21 @@ package LinkList;
 
 import bean.Student;
 
+import java.util.Scanner;
+
 /**
  * @author：ljj0452@gmail.com
  * @time: 2020/7/4 22:13
  */
 public class StudentSvc {
-    private StudentNode head;
+    private static StudentNode head = new StudentNode();
 
     // 添加学生
     public void addStudent(Student student) {
         // 初始化插入的student对象
         StudentNode newSNode = new StudentNode(student);
         // 暂存头节点
-        StudentNode temp = this.head;
+        StudentNode temp = head;
         // 找到尾节点
         while (temp.getNext() != null) {
             temp = temp.getNext();
@@ -24,7 +26,7 @@ public class StudentSvc {
 
     // 遍历链表
     public void showAllStudents() {
-        StudentNode temp = this.head;
+        StudentNode temp = head;
         int index = 1;
         while (temp != null) {
             if (temp.getData() != null) {
@@ -36,12 +38,35 @@ public class StudentSvc {
 
     // 索引删除学生
     public void delStudentByIndex(int index) {
-        StudentNode temp = this.head;
+        if (index < 1) {
+            System.out.println("需要输入正确的索引值！");
+            return;
+        }
+        StudentNode temp = head;
         int nowPos = 0;
         while (temp.getNext() != null) {
             if (nowPos == index - 1) {
                 StudentNode delNode = temp.getNext();
                 temp.setNext(delNode.getNext());
+                return;
+            }
+            nowPos++;
+            temp = temp.getNext();
+        }
+    }
+
+    // 索引修改学生
+    public void updateStudentByIndex(int index, Student student) {
+        if (index < 1) {
+            System.out.println("需要输入正确的索引值！");
+            return;
+        }
+        Scanner sc = new Scanner(System.in);
+        StudentNode temp = head;
+        int nowPos = 0;
+        while (temp.getNext() != null) {
+            if (nowPos == index - 1) {
+                temp.getNext().setData(student);
                 return;
             }
             nowPos++;
