808c6b2b9f4fedffbf98b47113a6ba0ea0744fee
dreamTjx <1280445703@qq.com>
2020-07-08 11:28:40 +0800

    提交了提交了

---
+++ HrTjx/src/system/Main.java
@@ -0,0 +1,12 @@
+package system;
+import student.student;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("111");
+        student stu = new student();
+        stu.select();
+    }
+}
