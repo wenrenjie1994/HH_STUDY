0e1fa80631402688e9534c6cd4e9184590e20181
zhongshuzhi <542481524@qq.com>
2020-07-08 16:51:18 +0800

    create new branch zhongshuzhi1779

---
+++ HR_SYS/src/bean/BookList.java
@@ -0,0 +1,16 @@
+package bean;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+
+public class BookList extends LinkedList<Book> {
+
+}
+
+//这里集成LinkedList和ArrayList的区别是什么
+
+//public class BookList extends ArrayList<Book> {
+//
+//}
