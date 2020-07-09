b775a0478a52ec149868c32fb4189190f319688b
KevinLJJ <ljj0452@gmail.com>
2020-07-09 20:37:30 +0800

    add update/delete function.

--- HRS/src/main/java/FileIO/FileOperation.java
+++ HRS/src/main/java/FileIO/FileOperation.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+import static Commons.StudentUtils.buildStudent;
 import static Commons.StudentUtils.showMenu;
 
 /**
@@ -13,7 +14,7 @@ import static Commons.StudentUtils.showMenu;
 public class FileOperation {
     public final static String fileName = "src/main/resources/data.txt";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         showMenu();
         while (true) {
@@ -23,6 +24,14 @@ public class FileOperation {
                     break;
                 case 2:
                     OperationSvc.addStudent(fileName);
+                    break;
+                case 3:
+                    System.out.print("输入需要删除的学生学号：");
+                    OperationSvc.delStudentByStuID(fileName, sc.next());
+                    break;
+                case 4:
+                    System.out.print("输入需要更新的学生学号：");
+                    OperationSvc.updateStudent(fileName, sc.next(), buildStudent());
                     break;
                 case 0:
                     System.exit(0);
--- HRS/src/main/java/FileIO/OperationSvc.java
+++ HRS/src/main/java/FileIO/OperationSvc.java
@@ -14,24 +14,29 @@ import static Commons.StudentUtils.buildStudent;
 public class OperationSvc {
 
     // 从文件中读学生列表
-    public static ArrayList<Student> getStudents(String fileName) throws IOException {
-        FileReader reader;
-        reader = new FileReader(fileName);
-        BufferedReader bufferedReader = new BufferedReader(reader);
-        String line;
+    public static ArrayList<Student> getStudents(String fileName) {
         ArrayList<Student> arrStudents = new ArrayList<Student>();
-        while ((line = bufferedReader.readLine()) != null) {
-            Student student = new Student();
-            // line format: stuID-name-age-major-interviewScore
-            String[] students = line.split("-");
-            student.setStuID(students[0]);
-            student.setName(students[1]);
-            student.setAge(Integer.parseInt(students[2]));
-            student.setMajor(students[3]);
-            student.setInterviewScore(Integer.parseInt(students[4]));
-            arrStudents.add(student);
+        try {
+            FileReader reader;
+            reader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                Student student = new Student();
+                // line format: stuID-name-age-major-interviewScore
+                String[] students = line.split("-");
+                student.setStuID(students[0]);
+                student.setName(students[1]);
+                student.setAge(Integer.parseInt(students[2]));
+                student.setMajor(students[3]);
+                student.setInterviewScore(Integer.parseInt(students[4]));
+                arrStudents.add(student);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("文件读取错误。");
         }
-        bufferedReader.close();
         return arrStudents;
     }
 
@@ -60,13 +65,13 @@ public class OperationSvc {
     }
 
     // 输出学生列表, 象征性输出一下
-    public static void showAllStudents(String fileName) throws IOException {
+    public static void showAllStudents(String fileName) {
         System.out.println(getStudents(fileName));
 
     }
 
     // 添加一个学生
-    public static void addStudent(String fileName) throws IOException {
+    public static void addStudent(String fileName) {
         // 文件读到学生数组
         ArrayList<Student> arrStudents = getStudents(fileName);
         Student newStudent = buildStudent();
@@ -78,6 +83,57 @@ public class OperationSvc {
         }
         arrStudents.add(newStudent);
         saveStudents2File(fileName, arrStudents);
-        System.out.println("添加学生: " + newStudent.getName() + "成功。");
+        System.out.println("添加学生: " + newStudent.getName() + " 成功。");
+    }
+
+    // 修改学生
+    public static void updateStudent(String fileName, String stuID, Student student) {
+        ArrayList<Student> arrStudents = getStudents(fileName);
+        // foreach flag
+        int index = 0;
+
+        //search student
+        for (Student temp : arrStudents) {
+            if (temp.getStuID().equals(stuID)) {
+                // update
+                arrStudents.set(index, student);
+                // save to file
+                saveStudents2File(fileName, arrStudents);
+                System.out.println("学生信息更新成功。");
+                return;
+            }
+            index++;
+            // stuID not found
+            if (index == arrStudents.size()) {
+                System.out.println("该学生不存在！");
+                return;
+            }
+        }
+    }
+
+    // 删除学生（使用学号）
+    public static void delStudentByStuID(String fileName, String stuID) {
+        ArrayList<Student> arrStudents = getStudents(fileName);
+        // foreach flag
+        int index = 0;
+
+        //search student
+        for (Student temp : arrStudents) {
+            if (temp.getStuID().equals(stuID)) {
+                // remove
+                arrStudents.remove(index);
+                // save to file
+                saveStudents2File(fileName, arrStudents);
+                System.out.println("删除学生:" + temp.getName() + " 成功。");
+                return;
+            }
+            index++;
+            // stuID not found
+            if (index == arrStudents.size()) {
+                System.out.println("该学生不存在！");
+                return;
+            }
+        }
+
     }
 }
