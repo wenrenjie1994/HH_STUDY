52a682c81479bd041a6875b2cc5d0dadbce22869
chen <chen@chen.com>
2020-07-12 16:11:02 +0800

    config files

--- hr_system1.0/src/HRSystem.java
+++
@@ -1,8 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * @Author: chenlei
- * @Date: 2020/7/12 14:56
- */
-public class HRSystem {
-}
--- hr_system1.0/src/Resume.java
+++
@@ -1,8 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * @Author: chenlei
- * @Date: 2020/7/12 15:00
- */
-public class Resume {
-}
