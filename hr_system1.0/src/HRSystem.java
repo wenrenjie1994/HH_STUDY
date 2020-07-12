3623405a819a2e424d42b287ab367ec8d9ac9efc
chen <chen@chen.com>
2020-07-12 16:09:48 +0800

    chen3stones hr system 1.0

---
+++ hr_system1.0/src/HRSystem.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @Author: chenlei
+ * @Date: 2020/7/12 14:56
+ */
+public class HRSystem {
+}
---
+++ hr_system1.0/src/Resume.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @Author: chenlei
+ * @Date: 2020/7/12 15:00
+ */
+public class Resume {
+}
