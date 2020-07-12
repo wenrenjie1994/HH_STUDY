b2e575e3b545835e399090f1b95d523956c6222b
xiaoxiuzhuo <1015981305@qq.com>
2020-07-12 21:39:15 +0800

    修复Vue.js中的事件响应问题

--- HR_SYS_DEMO_browser-server/src/main/java/com/example/hr_sys_demo_browserserver/controller/ResumeController.java
+++ HR_SYS_DEMO_browser-server/src/main/java/com/example/hr_sys_demo_browserserver/controller/ResumeController.java
@@ -6,10 +6,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -19,21 +16,49 @@ public class ResumeController {
     @Autowired ResumeService resumeService;
 
     /*restful 部分*/
-    @RequestMapping("/resumes")
+    @GetMapping ("/resumes")
     public PageInfo<Resume> list(@RequestParam(value = "start", defaultValue = "1") int start,@RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
         PageHelper.startPage(start, size, "id asc");
         List<Resume> rs = resumeService.list();
+        System.out.println(start);
         System.out.println(rs.size());
 
         PageInfo<Resume> page = new PageInfo<>(rs,5); //5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
 
         return page;
     }
+    @GetMapping ("/resumes/{id}")
+    public Resume get(@PathVariable("id") int id) throws Exception{
+        System.out.println(id);
+        Resume r = resumeService.get(id);
+        System.out.println(r);
+        return r;
+    }
+    @PostMapping("/resumes")
+    public String add(@RequestBody Resume r) throws Exception {
+        resumeService.add(r);
+        return "success";
+    }
+    @DeleteMapping("/resumes/{id}")
+    public String delete(Resume r) throws Exception{
+        resumeService.delete(r.getId());
+        return "success";
+    }
+    @PutMapping("/resumes/{id}")
+    public String update(@RequestBody Resume r) throws Exception{
+        resumeService.update(r);
+        return "success";
+    }
 
     /*页面跳转 部分*/
     @RequestMapping(value="/home", method= RequestMethod.GET)
     public ModelAndView listResume(){
         ModelAndView mv = new ModelAndView("home");
+        return mv;
+    }
+    @RequestMapping(value = "/editResume", method = RequestMethod.GET)
+    public ModelAndView editResume(){
+        ModelAndView mv = new ModelAndView("editResume");
         return mv;
     }
 }
