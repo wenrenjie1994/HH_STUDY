be6e4644fb988ec0c3960d649b1cfa75b6c556c9
internw <759448435@qq.com>
2020-07-13 21:37:33 +0800

    包装类型，JavaBean，枚举类，记录类，BigInteger，BigDecimal，常用工具类

--- JAVA基础/CoreClass/src/CoreClass.java
+++ JAVA基础/CoreClass/src/CoreClass.java
@@ -31,14 +31,71 @@ public class CoreClass{
 		System.out.println(sb);
 		*/
 		
+		/*
 		Adder adder = new Adder();
 		adder.add(5).inc();
 		System.out.printf("sum = %d",adder.value());
+		*/
 		
+		/*
+		int i = 10;
+		Integer n = new Integer(i);
+		Integer n2 = Integer.valueOf(i);
+		Integer n3 = Integer.valueOf("10");
+		System.out.printf("n = %d\nn2 = %d\nn3 = %d",n,n2,n3);
+		*/
+		
+		/*
+		// 向上转型为Number:
+		Number num = new Integer(999);
+		// 获取byte, int, long, float, double:
+		byte b = num.byteValue();
+		int n = num.intValue();
+		long ln = num.longValue();
+		float f = num.floatValue();
+		double d = num.doubleValue();
+		System.out.println(b);
+		System.out.println(n);
+		System.out.println(ln);
+		System.out.println(f);
+		System.out.println(d);*/
+		
+		/*
+		Weekday day = Weekday.TUE;
+		Weekday day2 = Weekday.THU;
+		System.out.println(day.getDayvalue());
+		System.out.println(day2.getDayvalue());
+		System.out.println(day.getChinese());
+		System.out.println(day.name());
+		System.out.println(day.equals(day2));
+		System.out.println(day.ordinal());
+		System.out.println(day2.ordinal());
+		*/
 		return;
 	
 	}
 }
+
+
+enum Weekday{
+	MON(0, "星期一"), TUE(1, "星期二"), WED(2, "星期三"), 
+	THU(3, "星期四"), FRI(4, "星期五"), SAT(5, "星期六"), 
+	SUN(6, "星期日");
+	private final int dayvalue;
+	private final String chinese;
+	
+	private Weekday(int dayvalue,String chinese) {
+		this.dayvalue = dayvalue;
+		this.chinese = chinese;
+	}
+	public int getDayvalue() {
+		return dayvalue;
+	}
+	public String getChinese() {
+		return chinese;
+	}
+}
+
 class Adder{
 	private int sum = 0;
 	
@@ -56,5 +113,22 @@ class Adder{
         return sum;
     }
 
+}
+//JavaBean
+class Person {
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	private String name;
+    private int age;
 }
 
