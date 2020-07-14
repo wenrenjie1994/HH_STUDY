29be463f01efd7834216ac4f362d06bfd0fe7ea0
tongguanyouwei <caoruidkd@163.com>
2020-07-14 08:29:10 +0800

    IO流的学习

--- JavaStudy/IO/src/FileCopy.java
+++ JavaStudy/IO/src/FileCopy.java
@@ -1,5 +1,29 @@
+import java.io.*;
+
 /**
  * 文件的复制
  */
 public class FileCopy {
+    public static void main(String[] args) {
+        //先建立两个file对象
+        File file=new File("IO\\caoruitest.txt");
+        File copy=new File("IO\\caoruitestCopy.txt");
+        //创建输入流和输出流，一边读取，一边写入
+        byte[] b=new byte[1024];
+
+        try (FileInputStream fis = new FileInputStream(file);
+             FileOutputStream fos = new FileOutputStream(copy);){
+            int len=0;
+            while((len=fis.read(b))!=-1){
+                fos.write(b,0,len);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
