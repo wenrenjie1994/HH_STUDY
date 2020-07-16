1adbfce819f8909bb58a7dacf30c8ef34d5c5d72
xiaoxiuzhuo <1015981305@qq.com>
2020-07-16 16:23:16 +0800

    通过FixedThreadPool来处理并发的网络连接请求

--- HR_SYS_DEMO_client-server/server/com/hr/Control.java
+++ HR_SYS_DEMO_client-server/server/com/hr/Control.java
@@ -5,18 +5,23 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.ResultSet;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Control {
     public Control() throws Exception{
         //建立服务连接
         ServerSocket serverSocket = new ServerSocket(88);
+        ExecutorService es = Executors.newFixedThreadPool(10);
         while (true){
             System.out.println("等待连接...");
             Socket socket = serverSocket.accept();
             System.out.println("收到新连接!");
-            Thread resumeTask = new Thread(new ResumeTask(socket));
+            //Thread resumeTask = new Thread(new ResumeTask(socket));
             System.out.println("Socket 线程：" + socket.toString() + " 开启");
-            resumeTask.start();
+            es.execute(new ResumeTask(socket));
+            //resumeTask.start();
         }
 
     }
