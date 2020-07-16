441bb6a701c4c7655b93f5ce6e3ebb4399ddb002
jiangjunhao <422367430@qq.com>
2020-07-17 01:21:24 +0800

    add new files

---
+++ Projects1/SelfTaught/HR_SYS/src/bean/Resume.java
@@ -0,0 +1,82 @@
+package bean;
+
+public class Resume {
+    //姓名
+    private String name;
+    //身份证号
+    private String id;
+    //学校
+    private String school;
+    /**
+     * 简历状态
+     * 0：已申请
+     * 1：简历通过
+     * 2：笔试通过
+     * 3：体检通过
+     * 4：背调通过
+     * 5：已签约
+     * 6：已入职
+     */
+    private int process;
+    //逻辑删除
+    private int deleteStatus;
+
+    public Resume(String name, String id, String school) {
+        this.name = name;
+        this.id = id;
+        this.school = school;
+        this.process = 0;
+        this.deleteStatus = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public int getProcess() {
+        return process;
+    }
+
+    public void setProcess(int process) {
+        this.process = process;
+    }
+
+    public int getDeleteStatus() {
+        return deleteStatus;
+    }
+
+    public void setDeleteStatus(int deleteStatus) {
+        this.deleteStatus = deleteStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Resume{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", school='" + school + '\'' +
+                ", process=" + process +
+                ", deleteStatus=" + deleteStatus +
+                '}';
+    }
+}
