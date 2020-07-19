576f244399327d8ab3263bb681bae67331f5def6
tongguanyouwei <caoruidkd@163.com>
2020-07-19 12:34:38 +0800

    HR系统第四版服务器（多线程）

--- HR_4_CAORUI/src/com/caorui/hr/HandleThread.java
+++ HR_4_CAORUI/src/com/caorui/hr/HandleThread.java
@@ -16,17 +16,14 @@ public class HandleThread extends Thread{
             OutputStream os=socket.getOutputStream();
             DataInputStream dis=new DataInputStream(is);
             DataOutputStream dos=new DataOutputStream(os);
-            while (true) {
-                System.out.println("新一轮的server开始工作啦");
-                //通过socket对象获取输入流
-                //接收客户端传来的url
-                String url =dis.readUTF();
-                System.out.println("2、"+url);
-                //解析输出的第一个数字，判断要进行哪个操作
-                String[] strings = url.split("/n");
-                String re = choice(strings);//获取操作的返回信息
-                dos.writeUTF(re);//将返回信息返回到客户端
-            }
+            //通过socket对象获取输入流
+            //接收客户端传来的url
+            String url =dis.readUTF();
+            System.out.println("2、"+url);
+            //解析输出的第一个数字，判断要进行哪个操作
+            String[] strings = url.split("/n");
+            String re = choice(strings);//获取操作的返回信息
+            dos.writeUTF(re);//将返回信息返回到客户端
         } catch (IOException e) {
             e.printStackTrace();
         }
