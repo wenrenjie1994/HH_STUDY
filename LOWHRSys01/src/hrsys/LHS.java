a0bf10117c1113dd8d89e00f655b0b6cc6182db8
yangjuan4554 <JuanYangCQU@163.com>
2020-07-22 17:18:40 +0800

    基于控制台的版本

---
+++ LOWHRSys01/src/hrsys/LHS.java
@@ -0,0 +1,55 @@
+package hrsys;
+
+import model.Candidate;
+import service.CandidateService;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LHS {
+    public static void printHRSys() {
+        System.out.println("============欢迎来到简单招聘系统1.0==============");
+        System.out.println("增加应聘者请输入-----1");
+        System.out.println("删除应聘者请输入-----2");
+        System.out.println("更新应聘者请输入-----3");
+        System.out.println("查询应聘者请输入-----4");
+        System.out.println("打印应聘者列表请输入-----5");
+        System.out.println("退出招聘系统-----0");
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Candidate> candidates = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean flag = true;//用于标志是否退出系统
+        while (flag) {
+            printHRSys();
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    CandidateService.addCandidate(candidates);
+                    break;
+                case 2:
+                    CandidateService.deleteCandidate(candidates);
+                    break;
+                case 3:
+                    CandidateService.updateCandidate(candidates);
+                    break;
+                case 4:
+                    CandidateService.searchCandidate(candidates);
+                    break;
+                case 5:
+                    CandidateService.printCandidates(candidates);
+                    break;
+                case 0:
+                    System.out.println("感谢使用简单招聘系统1.0，已退出...");
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("输入格式错处，请重新输入！");
+                    printHRSys();
+                    break;
+            }
+        }
+    }
+}
---
+++ LOWHRSys01/src/model/Candidate.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Candidate {
+
+    /**
+     * 应聘者信息包含：姓名，年龄，性别，电话
+     */
+    private String name;
+    private int age;
+    private String gender;
+    private String phoneNumber;
+
+    public Candidate(String name, int age, String gender, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+}
