7c9ed0d3b855969fba9a34b7bd0cebf9d2b41670
fanfuni <2205436513@qq.com>
2020-07-22 22:23:41 +0800

    10%

---
+++ HR_SYS_ver2.0/src/pojo/Resume.java
@@ -0,0 +1,65 @@
+package pojo;
+
+/**
+ * User: Alex FAN
+ * Date: 2020/7/22
+ * Time: 20:57
+ * Description:
+ */
+public class Resume {
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 身份证
+     */
+    private String id;
+    /**
+     * 学校
+     */
+    private String school;
+    /**
+     * 简历情况
+     * 0 申请中
+     * 1 简历通过
+     * 2 笔试通过
+     * 3 面试通过
+     * 4 签约
+     * 5 入职
+     */
+    private int flag;
+    public Resume(String name, String id, String school, int flag) {
+        this.name = name;
+        this.id = id;
+        this.school = school;
+        this.flag = flag;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getSchool() {
+        return school;
+    }
+    public void setSchool(String school) {
+        this.school = school;
+    }
+    public int getFlag() {
+        return flag;
+    }
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+    }
+
+
+
