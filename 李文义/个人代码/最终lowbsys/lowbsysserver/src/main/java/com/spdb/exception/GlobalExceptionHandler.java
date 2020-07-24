c267f26e016cc6f9411f573df6a47ef93674b9a0
pipiomaka <pipiomaka@qq.com>
2020-07-24 23:30:05 +0800

    lowbsys加入拦截器与全局异常处理

---
+++ 李文义/个人代码/最终lowbsys/lowbsysserver/src/main/java/com/spdb/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.spdb.exception;
+
+import com.spdb.response.Response;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author liwenyi
+ * @date 2020/7/24
+ * desc 全局异常处理器
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Response handleException(Exception exception) {
+        return new Response().failure(exception.toString());
+    }
+}
