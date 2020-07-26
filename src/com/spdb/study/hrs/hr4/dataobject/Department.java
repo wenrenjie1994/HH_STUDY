f044586bc9c59e72f53114539026dcd4fbed5df1
cafebabe <wxappdevelop@163.com>
2020-07-26 11:33:45 +0800

    实现HR4.0对员工绩效信息的增删改查，以及分页查询，其中重点掌握了Oracle的分页查询写法

---
+++ src/com/spdb/study/hrs/hr4/dataobject/Department.java
@@ -0,0 +1,50 @@
+package com.spdb.study.hrs.hr4.dataobject;
+
+import java.io.Serializable;
+
+/**
+ * 部门实体类
+ * @author Mr.Longyx
+ * @date 2020年07月20日 22:03
+ */
+
+public class Department implements Serializable {
+    private static final long serialVersionUID = -7605999156709087747L;
+
+    private int departmentId;
+    private int pid;
+    private String departmentName;
+    private int leaf;
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public int getLeaf() {
+        return leaf;
+    }
+
+    public void setLeaf(int leaf) {
+        this.leaf = leaf;
+    }
+}
